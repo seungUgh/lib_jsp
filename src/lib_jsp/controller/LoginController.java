db40ab7de65c5442ee4b445ed8aa5a94fd314f2e
seungeun <tmddms0931@naver.com>
2020-09-16 17:35:33 +0900

    초기화 작업중....

---
+++ src/lib_jsp/controller/LoginController.java
@@ -0,0 +1,27 @@
+package lib_jsp.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/LoginController")
+public class LoginController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public LoginController() {
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		process(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		process(request, response);
+	}
+	private void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
